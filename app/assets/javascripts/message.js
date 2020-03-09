de187aee7015e52cd0eda2f8efd9fe162d7fe5bf
osamu26 <60730317+osamu26@users.noreply.github.com>
2020-03-09 20:19:26 +0900

    児童更新　$()

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -23,55 +23,55 @@ $(function(){
     });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-  setInterval(reloadMessages, 7000)
+  setInterval(reloadMessages, 7000);
   }
 $(function(){ 
   var buildHTML = function(message) {
     if (message.content && message.image) {
-      var html = `<div class="message" data-message-id=  message.id >
+      var html = `<div class="message" data-message-id="${message.id}">
         <div class="upper-message"> 
           <div class="upper-message__user-name"> 
-            message.user_name  
+            ${message.user_name}  
           </div> 
           <div class="upper-message__date"> 
-            message.created_at 
+            ${message.created_at} 
           </div> 
         </div> 
         <div class="lower-message"> 
           <p class="lower-message__content"> 
-            message.content 
+            ${message.content} 
           </p> 
-          <img src=" + message.image " class="lower-message__image" > 
+          ${img}=" + message.image " class="lower-message__image" 
         </div> 
       </div>`
     } else if (message.content) {
-      var html = `<div class="message" data-message-id= + message.id  > 
+      var html = `<div class="message" data-message-id= "${message.id}"> 
         <div class="upper-message"> 
           <div class="upper-message__user-name"> 
-            message.user_name 
+            ${message.user_name} 
           </div> 
           <div class="upper-message__date"> 
-            message.created_at 
+            ${message.created_at} 
           </div> 
         <div> 
         <div class="lower-message"> 
           <p class="lower-message__content"> 
-            message.content 
+            ${message.content} 
           </p> 
         </div> 
       </div>`
     } else if (message.image) {
-      var html = `<div class="message" data-message-id= + message.id  > 
+      var html = `<div class="message" data-message-id= "${ message.id }" > 
         <div class="upper-message"> 
           <div class="upper-message__user-name"> 
-            message.user_name 
+            ${message.user_name} 
           </div> 
           <div class="upper-message__date"> 
-            message.created_at 
+            ${message.created_at} 
           </div> 
         </div> 
         <div class="lower-message"> 
-          <img src="  message.image  " class= "lower-message__image" > 
+          ${img}="  message.image  " class= "lower-message__image" > 
         </div> 
       </div>`
     };
