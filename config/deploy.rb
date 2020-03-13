16787aeb207533bad3d309d63395055f43e2c34a
osamu26 <60730317+osamu26@users.noreply.github.com>
2020-03-13 19:58:45 +0900

    不要なコード削除

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,12 +33,6 @@ namespace :deploy do
   end
 end
 
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv/",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:SPATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
 
   # secrets.yml用のシンボリックリンクを追加
 set :linked_files, %w{ config/secrets.yml }
